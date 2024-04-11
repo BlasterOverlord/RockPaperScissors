b3a06a56eccc4df8b2b16bd7978822dfc49cb421
BlasterOverlord <72990637+BlasterOverlord@users.noreply.github.com>
2024-04-11 20:50:28 +0600

    added scoring system!

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = ['Rock', 'Paper', 'Scissors'];
     return choice[Math.floor(Math.random() * 3)];
@@ -8,23 +11,38 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection == 'rock' && computerSelection == 'paper')
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore++;
         return 'You Lose! Paper beats Rock!';
-    else if (playerSelection == 'rock' && computerSelection == 'scissors')
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return 'You Win! Rock beats Scissors!';
-    else if (playerSelection == 'paper' && computerSelection == 'rock')
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return 'You Win! Paper beats Rock';
-    else if (playerSelection == 'paper' && computerSelection == 'scissors')
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore++;
         return 'You Lose! Scissors beat Paper!';
-    else if (playerSelection == 'scissors' && computerSelection == 'rock')
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore++;
         return 'You Lose! Rock beats Scissors';
-    else if (playerSelection == 'scissors' && computerSelection == 'paper')
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore++;
         return 'You Win! Scissors beat Paper!';
+    }
     else
         return "Alas! It is a draw!";
   }
   
-  const playerSelection = "rock";
+  const playerSelection = 'rock'; //prompt("Rock, Paper or Scissors?");
   const computerSelection = getComputerChoice();
+
   console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
   
